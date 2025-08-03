852b43540912471178e35d0c04fb902ef242dbd5
jboll3 <jboll3@gmail.com>
2025-08-03 09:51:32 -0400

    Add player movement functionality and update constants for speed

--- player.py
+++ player.py
@@ -2,7 +2,7 @@ import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
 from circleshape import *
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -30,3 +30,11 @@ class Player(CircleShape):
             self.rotate(dt*1)
         if keys[pygame.K_d]:
             self.rotate(dt*-1)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(dt*-1)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
