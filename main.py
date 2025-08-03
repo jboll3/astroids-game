b6cf24536c0e2b3844fd4ebe2009e886cb77025b
jboll3 <jboll3@gmail.com>
2025-08-03 09:38:11 -0400

    Add player and circle shape classes with movement and drawing functionality

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
 from constants import *
+from player import *
 
 def main():
     dt = 0
     pygame.init()    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    # Main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         dt_ms = clock.tick(60)
         dt = dt_ms / 1000.0
